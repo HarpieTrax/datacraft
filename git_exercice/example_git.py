c5011deed132e4f77fa7a7696c81fbb981de1ac4
Paul <paul.lachaise19@gmail.com>
2025-04-15 11:20:24 +0200

    teste2

--- git_exercice/example_git.py
+++ git_exercice/example_git.py
@@ -2,11 +2,7 @@ import typing as t
 from typing import List, Dict, Union
 
 
-<<<<<<< HEAD
 name = "paul"   #change with your name ;)
-=======
-name = ""   #change with your name ;)
->>>>>>> 7989a715a8d0cdc16fa8ffd1164869ccb5004718
 
 if name == "":
     print("there is no one here...")
